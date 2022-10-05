dfed85e8117b6695c1ed169f9c5388e93af02924
gfrise <104461249+gfrise@users.noreply.github.com>
2022-10-05 16:54:14 +0200

    props-04

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import Hello from './Hello';
 import Welcome from './Welcome';
 
 class App extends React.Component {
     render () {
-        return <Welcome name={'Joe'} age={25} />
+        return <Welcome name={<strong>Joe</strong>} />
     }
 }
 
