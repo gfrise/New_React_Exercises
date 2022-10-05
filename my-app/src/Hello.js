7729f3d562f8a0ed7392d9f8a20808a711ad8ef3
gfrise <104461249+gfrise@users.noreply.github.com>
2022-10-05 16:24:03 +0200

    components-06

--- my-app/src/Hello.js
+++ my-app/src/Hello.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 class Hello extends React.Component {
     render () {
-        return <h1>Hello</h1>
+        return (
+            <>
+                <h1>Hello</h1>
+                <p>What a beautiful day!</p>
+            </>
+        )
+         
     }
 } 
 
