03df22b25b989bc5d9104909b15846f386053095
gfrise <104461249+gfrise@users.noreply.github.com>
2022-10-05 16:20:57 +0200

    components-04

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,5 +1,4 @@
 import ReactDOM from 'react-dom';
 import App from './Hello';
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+ReactDOM.render(<App />, document.getElementById('root'))
