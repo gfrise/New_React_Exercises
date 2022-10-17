0e48219ac2d9781b8970672eed2ef8ae872f97ef
gfrise <104461249+gfrise@users.noreply.github.com>
2022-10-17 16:29:14 +0200

    style1

--- my-app/src/Welcome.js
+++ my-app/src/Welcome.js
@@ -6,10 +6,10 @@ export const name = <strong>Giuseppe</strong>
 export class Welcome extends React.Component {
     render() {
         return (
-            <>
+            <div className='welcome'>
                 <p>Welcome {this.props.name}</p>
                 {this.props.age > 18 && this.props.age < 65 && this.props.name === "John" && <Age age={this.props.age} />}
-            </>
+            </div>
         )
     }
 }
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,5 +1,6 @@
 import { App } from './App';
 import ReactDOM from 'react-dom';
+import './index.css'
 
 const root = document.querySelector('#root');
 ReactDOM.render(<App />,
