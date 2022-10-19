3b708615ba62723153af6979e5fbccfcd778eb05
gfrise <104461249+gfrise@users.noreply.github.com>
2022-10-19 18:06:07 +0200

    function1

--- my-app/src/Welcome.js
+++ my-app/src/Welcome.js
@@ -3,13 +3,22 @@ import { Age } from './Age';
 
 export const name = <strong>Giuseppe</strong>
 
-export class Welcome extends React.Component {
-    render() {
-        return (
-            <div className='welcome'>
-                <p>Welcome {this.props.name}</p>
-                {this.props.age > 18 && this.props.age < 65 && this.props.name === "John" && <Age age={this.props.age} />}
-            </div>
-        )
-    }
-}
+// export class Welcome extends React.Component {
+//     render() {
+//         return (
+//             <div className='welcome'>
+//                 <p>Welcome {this.props.name}</p>
+//                 {this.props.age > 18 && this.props.age < 65 && this.props.name === "John" && <Age age={this.props.age} />}
+//             </div>
+//         )
+//     }
+// }
+
+export function Welcome(props) {
+    return (
+        <div className='welcome'>
+            <p>Welcome {props.name}</p>
+            {props.age > 18 && props.age < 65 && props.name === "John" && <Age age={props.age} />}
+        </div>
+    )
+} 
