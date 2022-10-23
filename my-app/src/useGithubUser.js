5c2525a3bf4aaf99ceb6bd9adb6a688445daee8d
gfrise <104461249+gfrise@users.noreply.github.com>
2022-10-23 17:31:16 +0200

    hooks4

--- my-app/src/useGithubUser.js
+++ my-app/src/useGithubUser.js
@@ -2,19 +2,23 @@ import React, { useState, useEffect } from "react";
 
 export function useGithubUser({username}) {
     const [data, setData] = useState([])
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
+    setLoading(true);
         fetch(`https://api.github.com/users/${username}`)
-        .then(response => {
-            return response.json()
-        })
-        .then(json => {
-            return setData(json)
-        })
+        .then(response => {return response.json()})
+        .then(data => setData(data))
+        .catch(error => setError(error))
+        .then(json => {return setData(json)})
+        .finally(()=> setLoading(false))
     }, [username])
 
     return {
         name: data.name,
-        login: data.login
+        login: data.login,
+        loading,
+        error,
     }
 }
