d693d4c880b66cc3902dcc24e9a4d9946d361078
gfrise <104461249+gfrise@users.noreply.github.com>
2022-11-06 16:35:59 +0100

    swr1

--- my-app/src/useGithubUser.js
+++ my-app/src/useGithubUser.js
@@ -1,24 +1,14 @@
 import React, { useState, useEffect } from "react";
+import useSWR from 'swr'
 
-export function useGithubUser({username}) {
-    const [data, setData] = useState([])
-    const [error, setError] = useState(null)
-    const [loading, setLoading] = useState(false)
+const fetcher = (url) => fetch(url).then((res) => res.json())
 
-    useEffect(() => {
-    setLoading(true);
-        fetch(`https://api.github.com/users/${username}`)
-        .then(response => {return response.json()})
-        .then(data => setData(data))
-        .catch(error => setError(error))
-        .then(json => {return setData(json)})
-        .finally(()=> setLoading(false))
-    }, [username])
+export function useGithubUser(username) {
+    const {data, error} = useSWR(`https://api.github.com/users/${username}`, fetcher)
 
     return {
         name: data.name,
         login: data.login,
-        loading,
-        error,
+        error
     }
 }
