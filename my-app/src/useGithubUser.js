f8df8f684529fea5ca22793ddd53cda65053440b
gfrise <104461249+gfrise@users.noreply.github.com>
2022-11-06 16:39:04 +0100

    swr2

--- my-app/src/useGithubUser.js
+++ my-app/src/useGithubUser.js
@@ -4,7 +4,7 @@ import useSWR from 'swr'
 const fetcher = (url) => fetch(url).then((res) => res.json())
 
 export function useGithubUser(username) {
-    const {data, error} = useSWR(`https://api.github.com/users/${username}`, fetcher)
+    const {data, error} = useSWR(username ? `https://api.github.com/users/${username}` : null, fetcher)
 
     return {
         name: data.name,
